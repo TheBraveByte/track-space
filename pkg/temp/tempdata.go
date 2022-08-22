4c617750e646f7a1ad92529b21b4999302798b62
dev-ayaa <ayaaakinleye@gmail.com>
2022-08-22 02:18:56 +0100

    new struct field

--- pkg/temp/tempdata.go
+++ pkg/temp/tempdata.go
@@ -1,12 +1,11 @@
 package temp
 
-
-//probably wont be making use of this package
+// probably wont be making use of this package
 type TemplateData struct {
-	StringData   map[string]string
-	IntData      map[string]int
-	Float64Data  map[string]float64
-	Error        string
-	FlashMessage string
-	RandomData   map[string]interface{}
+	StringData      map[string]string
+	IntData         map[string]int
+	Float64Data     map[string]float64
+	Error           string
+	RandomData      map[string]interface{}
+	IsAuthenticated int
 }
